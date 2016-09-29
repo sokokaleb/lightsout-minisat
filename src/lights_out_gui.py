f4883a21eecc0243c3c283468014a4ea8c948528
Pusaka Kaleb Setyabudi <sokokaleb@gmail.com>
2016-09-29 23:08:12 +0700

    sampah

--- src/lights_out_gui.py
+++ src/lights_out_gui.py
@@ -1,12 +1,21 @@
-# TODO(athin2008): Finish this implementation
+from Tkinter import *
+import ttk
+
+from src.util.board_configuration import BoardConfiguration
 
 class LightsOutGUI(object):
     def __init__(self, minisat_wrapper=None):
         if minisat_wrapper is None:
             raise Exception('minisat_wrapper should be filled!')
         self.minisat_wrapper = minisat_wrapper
+        self.board_config = BoardConfiguration(3, 3)
 
     def run(self):
         print 'I\'m running!' # Erase this once the function is implemented
-        # TODO(athin2008): Finish this function
-        pass
+        root = Tk()
+        
+        content = ttk.Frame(root)
+
+        content.grid(column=0, row=0)
+
+        root.mainloop()
